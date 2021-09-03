9547163bbca34af3e9c3a0a25ea4a63a1fb55780
Cristian Montes Montecinos <cristian.montes@my.simpson.edu>
2021-09-03 12:14:07 -0500

    Lab1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,5 @@
+print("Hello, my name is Cristian Montecinos.")
+print("I have been in Des Moines Iowa for 19 years, My parents came from El Salvador down in Central America.")
+print("One day, my Grandma told me, \"You are very lucky to be born here\". At the beggining, I did not understand why she felt that way."
+      "\nUntil one day I saw the news and The news reporter was explaining the gang violence back in El Salvador." )
+print("Ever since then, I have never thought about going to El Salvador until its \"safe\".")
